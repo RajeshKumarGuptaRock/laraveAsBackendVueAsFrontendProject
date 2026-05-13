40be9f12817a0a310ab10fe16014339974e3df15
Rajesh Kumar Gupta <rkumargupta798@gmail.com>
2026-05-13 13:02:57 +0530

    fresh laravel project

--- backend/blog/vite.config.js
+++ backend/blog/vite.config.js
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { bunny } from 'laravel-vite-plugin/fonts';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-    },
-
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            fonts: [
+                bunny('Instrument Sans', {
+                    weights: [400, 500, 600],
+                }),
+            ],
         }),
-
-        vue(),
+        tailwindcss(),
     ],
-})
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+});
